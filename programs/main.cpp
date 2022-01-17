db644da8d4b2dc58a01a4be23c553077ed155770
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2022-01-17 18:12:16 +0600

    Update main.cpp

--- programs/main.cpp
+++ programs/main.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x)          (x).begin(),(x).end()
+#define MOD             1000000007
+#define case(tc)        cout << "Case " << tc << ": ";
+#define close           << "\n";
+typedef int64_t ll;
+typedef uint64_t ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
+void test(int tc){
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#ifdef necromancer
+    freopen("./input.txt",  "r", stdin);
+    // freopen("./output.txt", "w", stdout);
+    // freopen("./error.txt",  "w", stderr);
+#endif
+    int testcase = 1;
+    // cin >> testcase;
+    for (int tc = 0; tc < testcase; tc++) {
+        test(tc);
+    }
+}
